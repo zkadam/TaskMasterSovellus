ce96814f1a6ff8e06c96c8f4924a13d90bf3824b
zkadam <adam.zakar@student.careeria.fi>
2020-06-12 09:31:21 +0300

    css muotoilut, sprint task list filteroitu

---
+++ TaskMasterSovellus/Controllers/SprintTemplateConnectionsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TaskMasterSovellus.Models;
+
+namespace TaskMasterSovellus.Controllers
+{
+    public class SprintTemplateConnectionsController : Controller
+    {
+        private TaskMasterTietokantaEntities db = new TaskMasterTietokantaEntities();
+
+        // GET: SprintTemplateConnections
+        public ActionResult Index()
+        {
+            var sprintTemplateConnection = db.SprintTemplateConnection.Include(s => s.SprintTemplate).Include(s => s.Sprints);
+            return View(sprintTemplateConnection.ToList());
+        }
+
+        // GET: SprintTemplateConnections/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SprintTemplateConnection sprintTemplateConnection = db.SprintTemplateConnection.Find(id);
+            if (sprintTemplateConnection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sprintTemplateConnection);
+        }
+
+        // GET: SprintTemplateConnections/Create
+        public ActionResult Create()
+        {
+            ViewBag.SprintTemplateId = new SelectList(db.SprintTemplate, "SprintTemplateId", "SprintTemplateName");
+            ViewBag.SprintId = new SelectList(db.Sprints, "SprintId", "SprintName");
+            return View();
+        }
+
+        // POST: SprintTemplateConnections/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TemplateConnectionId,SprintId,SprintTemplateId")] SprintTemplateConnection sprintTemplateConnection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SprintTemplateConnection.Add(sprintTemplateConnection);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SprintTemplateId = new SelectList(db.SprintTemplate, "SprintTemplateId", "SprintTemplateName", sprintTemplateConnection.SprintTemplateId);
+            ViewBag.SprintId = new SelectList(db.Sprints, "SprintId", "SprintName", sprintTemplateConnection.SprintId);
+            return View(sprintTemplateConnection);
+        }
+
+        // GET: SprintTemplateConnections/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SprintTemplateConnection sprintTemplateConnection = db.SprintTemplateConnection.Find(id);
+            if (sprintTemplateConnection == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SprintTemplateId = new SelectList(db.SprintTemplate, "SprintTemplateId", "SprintTemplateName", sprintTemplateConnection.SprintTemplateId);
+            ViewBag.SprintId = new SelectList(db.Sprints, "SprintId", "SprintName", sprintTemplateConnection.SprintId);
+            return View(sprintTemplateConnection);
+        }
+
+        // POST: SprintTemplateConnections/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TemplateConnectionId,SprintId,SprintTemplateId")] SprintTemplateConnection sprintTemplateConnection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sprintTemplateConnection).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SprintTemplateId = new SelectList(db.SprintTemplate, "SprintTemplateId", "SprintTemplateName", sprintTemplateConnection.SprintTemplateId);
+            ViewBag.SprintId = new SelectList(db.Sprints, "SprintId", "SprintName", sprintTemplateConnection.SprintId);
+            return View(sprintTemplateConnection);
+        }
+
+        // GET: SprintTemplateConnections/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SprintTemplateConnection sprintTemplateConnection = db.SprintTemplateConnection.Find(id);
+            if (sprintTemplateConnection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sprintTemplateConnection);
+        }
+
+        // POST: SprintTemplateConnections/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SprintTemplateConnection sprintTemplateConnection = db.SprintTemplateConnection.Find(id);
+            db.SprintTemplateConnection.Remove(sprintTemplateConnection);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TaskMasterSovellus/Controllers/SprintTemplatesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TaskMasterSovellus.Models;
+
+namespace TaskMasterSovellus.Controllers
+{
+    public class SprintTemplatesController : Controller
+    {
+        private TaskMasterTietokantaEntities db = new TaskMasterTietokantaEntities();
+
+        // GET: SprintTemplates
+        public ActionResult Index()
+        {
+            return View(db.SprintTemplate.ToList());
+        }
+
+        // GET: SprintTemplates/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SprintTemplate sprintTemplate = db.SprintTemplate.Find(id);
+            if (sprintTemplate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sprintTemplate);
+        }
+
+        // GET: SprintTemplates/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SprintTemplates/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SprintTemplateId,SprintTemplateName")] SprintTemplate sprintTemplate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SprintTemplate.Add(sprintTemplate);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sprintTemplate);
+        }
+
+        // GET: SprintTemplates/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SprintTemplate sprintTemplate = db.SprintTemplate.Find(id);
+            if (sprintTemplate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sprintTemplate);
+        }
+
+        // POST: SprintTemplates/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SprintTemplateId,SprintTemplateName")] SprintTemplate sprintTemplate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sprintTemplate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sprintTemplate);
+        }
+
+        // GET: SprintTemplates/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SprintTemplate sprintTemplate = db.SprintTemplate.Find(id);
+            if (sprintTemplate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sprintTemplate);
+        }
+
+        // POST: SprintTemplates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SprintTemplate sprintTemplate = db.SprintTemplate.Find(id);
+            db.SprintTemplate.Remove(sprintTemplate);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
